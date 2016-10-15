740497a92c523fce44c9796a7035972381e37c46
John Lockwood <John@CodeSolid.com>
2016-10-15 09:48:00 -0700

    Get mocha_starter working with typescript

--- mocha_starter/test/SampleTest.ts
+++ mocha_starter/test/SampleTest.ts
@@ -1,16 +1,20 @@
-import { expect } from 'chai';
+// import expect from 'expect';
+// See http://stackoverflow.com/questions/35812221/typescript-default-import-failing for 
+// the why of next two lines
+import * as _expect from 'expect';
+const expect = _expect as any as typeof _expect.default;
 
 describe('Sample', () => {
     describe('#hello world test', () => {
         it('Hello world should say hello world', () => {
             var actual : string = "Hello world";
-            expect(actual).to.equal("Hello world");            
+            expect(actual).toEqual("Hello world");            
         });
     });
 
     describe('#test something else', () => {
         it('Another test should work', () => {
-            expect(1).to.not.equal(9);
+            expect(1).toNotEqual(9);
         });
     });
 
